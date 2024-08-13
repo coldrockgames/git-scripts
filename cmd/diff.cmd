226c72b0f7164237f4bf92c00067c7f47303920b
Mike <mike.barthold@outlook.com>
2024-08-13 17:36:11 +0200

    cheat sheet updated for the new diff script

--- cmd/diff.cmd
+++ cmd/diff.cmd
@@ -1,22 +1,16 @@
 @ECHO OFF
 SET REPO=%1
 
-SET PR=
-IF [%2]==[-prev] SET PR=/startrev:PREV
-
 IF [%REPO%]==[] GOTO ERROR
 IF NOT EXIST %REPO% GOTO ERROR
 
 ECHO Launching tortoise diff window...
-start tortoisegitproc /command:diff /path:./%REPO% %PR%
+start tortoisegitproc /command:diff /path:./%REPO%
 GOTO END
 
 :ERROR
 ECHO Error: No repository specified or repository "%REPO%" does not exist.
 ECHO Usage: diff repo [-prev]
-ECHO This script will show you the tortoise diff view for the repo,
-ECHO which will compare your working space with the local repo. 
-ECHO Use -prev argument to compare with previous committed version
-ECHO (i.e. the version before the latest commit).
+ECHO This script will show you the tortoise diff view for the repo.
 
 :END
